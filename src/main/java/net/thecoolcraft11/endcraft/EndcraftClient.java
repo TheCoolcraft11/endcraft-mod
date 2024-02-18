60c966029e2f528698050e7b3d2c86b278c36892
TheCoolcraft11 <137944139+TheCoolcraft11@users.noreply.github.com>
2024-02-18 12:34:51 +0100

    Initial commit

---
+++ src/main/java/net/thecoolcraft11/endcraft/EndcraftClient.java
@@ -0,0 +1,10 @@
+package net.thecoolcraft11.endcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndcraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/thecoolcraft11/endcraft/EndcraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.thecoolcraft11.endcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EndcraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
